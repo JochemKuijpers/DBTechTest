862f96f1f7361d16943ae53d43261e68bd3bfc9f
Jochem Kuijpers <github.com@jochemkuijpers.nl>
2018-03-11 23:01:34 +0100

    Testing various parameters for the report

--- src/SimpleEstimator.cpp
+++ src/SimpleEstimator.cpp
@@ -136,7 +136,7 @@ cardStat SimpleEstimator::estimate(RPQTree *q) {
     auto *rightSamples = new std::vector<uint32_t>();
 
     double underSampling;
-    uint32_t MAX_SAMPLING = 64;
+    uint32_t MAX_SAMPLING = 256;
 
     // generate uniform sampling of the outVertices for the first label
     if (path[0].second) {
